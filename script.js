d83f5bb366bf7665bd778085d3fc0020bb6c2172
felippepg <felippepg.goncalves@outlook.com.br>
2022-07-07 06:41:55 -0300

    menu-responsivo

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const menuButton = document.querySelector('.cabecalho__menu-hamburger');
 const degrade = getComputedStyle(document.documentElement).getPropertyValue('--azul-degrade');
 const menuList = document.querySelector('.lista__menu');
 const optionList = document.querySelectorAll('.lista')
+
+const desktopMenuList = document.querySelector('.lista__menu__container')
+const menuCategoria = document.querySelector('.container__botao-categoria');
+const optionListDesktop = document.querySelectorAll('.lista__container');
+
 let active = false;
 
 
@@ -20,6 +25,33 @@ menuButton.addEventListener('click', () => {
 
 })
 
+menuCategoria.addEventListener('click', () => {
+
+    if (!active) {
+        menuCategoria.style.background = degrade;
+        menuCategoria.style.color = "white";
+        menuCategoria.style.padding = "0 0.5em";
+        desktopMenuList.style.display = "block";
+        active = true;
+    } else {
+        menuCategoria.style.background = "white";
+        menuCategoria.style.color = "black";
+        desktopMenuList.style.display = "none";
+        active = false;
+    }
+})
+
+optionListDesktop.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        optionListDesktop.forEach((another) => {
+            if (another.classList.contains('active__container')) {
+                another.classList.remove('active__container')
+            }
+        })
+        item.classList.add('active__container')
+    })
+
+})
 
 optionList.forEach((item) => {
     item.addEventListener('click', (e) => {
@@ -32,6 +64,7 @@ optionList.forEach((item) => {
     })
 })
 
+
 const swiper = new Swiper('.swiper', {
     speed: 400,
     spaceBetween: 10,
@@ -41,7 +74,6 @@ const swiper = new Swiper('.swiper', {
         delay: 5000,
     },
 
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets'
